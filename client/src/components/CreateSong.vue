9ff2af3c3c721a174429b3d415c5718b4b8c19ab
Stephen Herron <stephen.herron23@gmail.com>
2018-09-19 23:57:52 +0100

    Fleshing out the CreateSong component

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -1,17 +1,86 @@
 <template>
   <!-- <v-container fluid> -->
-    <v-layout column>
-      <app-panel title="Create Song">
+    <v-layout>
+      <v-flex xs4>
+        <app-panel title="Create Song">
+          <v-container slot="content">
+            <!-- <form> -->
+              <v-flex>
+                <v-text-field
+                  label="Title"
+                  v-model="song.title"
+                ></v-text-field>
+              </v-flex>
+              <br>
+              <v-flex>
+                <v-text-field
+                  label="Artist"
+                  v-model="song.artist"
+                ></v-text-field>
+              </v-flex>
+              <br>
+              <v-flex>
+                <v-text-field
+                  label="Genre"
+                  v-model="song.genre"
+                ></v-text-field>
+              </v-flex>
+              <br>
+              <v-flex>
+                <v-text-field
+                  label="Album"
+                  v-model="song.album"
+                ></v-text-field>
+              </v-flex>
+              <br>
+              <v-flex>
+                <v-text-field
+                  label="Album Image URL"
+                  v-model="song.albumImageUrl"
+                ></v-text-field>
+              </v-flex>
+              <br>
+              <v-flex>
+                <v-text-field
+                  label="YouTube ID"
+                  v-model="song.youtubeId"
+                ></v-text-field>
+              </v-flex>
 
-        <v-container slot="content">
-          <p v-for="song in songs" :key="song.id">
-            {{song.title}} - {{song.artist}}
-          </p>
-        </v-container>
-
-      </app-panel>
+              <!-- </form> -->
+          </v-container>
+        </app-panel>
+      </v-flex>
+      <v-flex xs8>
+        <app-panel title="Something else" class="pl-2">
+          <v-container slot="content">
+            <v-flex>
+              <v-textarea
+                label="Lyrics"
+                v-model="song.lyrics"
+              ></v-textarea>
+            </v-flex>
+            <v-flex>
+              <v-textarea
+                label="Tab"
+                v-model="song.tab"
+              ></v-textarea>
+            </v-flex>
+            <v-btn
+              class="indigo" dark
+              @click="createSong"
+            >Create Song
+            </v-btn>
+          </v-container>
+        </app-panel>
+        <v-container
+          ml-2 mt-2 mr-4 mb-2
+          v-html="error"
+          class="red lighten-4"
+          v-if="error"
+        />
+      </v-flex>
     </v-layout>
-  <!-- </v-container> -->
 </template>
 
 <script>
@@ -25,15 +94,24 @@ export default {
   },
   data() {
     return {
-      song: [
-
-      ],
+      error: 'dwadwadawdwa',
+      song: {
+        title: null,
+        artist: null,
+        genre: null,
+        album: null,
+        albumImageUrl: null,
+        youtubeId: null,
+        lyrics: null,
+        tab: null,
+      },
     };
   },
   methods: {
     async createSong() {
       try {
-        const response = await SongsService.post();
+        await SongsService.post();
+        this.song = null;
       } catch (error) {
         this.error = error.response.data.error;
       }
